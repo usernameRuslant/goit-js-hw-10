177050a7fe1b912a13cc8b7e96db0c6797c79877
Ruslan Tukhfyatullov <ruslan.tukhfyatullov@gmail.com>
2025-07-17 16:21:23 +0300

    add

--- src/js/timer-toast.js
+++ src/js/timer-toast.js
@@ -1,9 +1,13 @@
 import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
 
 export function showDateError() {
   iziToast.error({
     message: 'Please choose a date in the future',
     position: 'topRight',
+    backgroundColor: ' #ef4040;', // ярко-красный фон
+    messageColor: '#ffffff', // белый текст
+    titleColor: '#ffffff',
+    iconUrl: '../img/x.svg',
+    iconColor: '#fff',
   });
 }
